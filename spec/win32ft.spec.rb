7dfc292af7b4b76bc09904892360ffc92c7d248f
windwiny <windwiny.ubt@gmail.com>
2013-06-03 23:00:31 +0800

    win32 file time functions

---
+++ spec/win32ft.spec.rb
@@ -0,0 +1,210 @@
+$:.unshift ".."
+require "win32ft"
+
+describe "FileTime" do
+  it "new FileTime instance should ==" do
+    FileTime.new.should == FileTime.new
+  end
+  it "new FileTime instance should equal" do
+    FileTime.new.should equal(FileTime.new)
+  end
+  it "new FileTime instance should eql" do
+    FileTime.new.should eql(FileTime.new)
+  end
+  it "new FileTime instance should eq" do
+    FileTime.new.should eq(FileTime.new)
+  end
+end
+
+describe "SystemTime" do
+  it "new SystemTime instance should ==" do
+    SystemTime.new.should == SystemTime.new
+  end
+  it "new SystemTime instance should equal" do
+    SystemTime.new.should equal(SystemTime.new)
+  end
+  it "new SystemTime instance should eql" do
+    SystemTime.new.should eql(SystemTime.new)
+  end
+  it "new SystemTime instance should eq" do
+    SystemTime.new.should eq(SystemTime.new)
+  end
+end
+
+describe "Large_Integer" do
+  it "new Large_Integer instance should ==" do
+    Large_Integer.new.should == Large_Integer.new
+  end
+  it "new Large_Integer instance should equal" do
+    Large_Integer.new.should equal(Large_Integer.new)
+  end
+  it "new SystemTime instance should eql" do
+    Large_Integer.new.should eql(Large_Integer.new)
+  end
+  it "new SystemTime instance should eq" do
+    Large_Integer.new.should eq(Large_Integer.new)
+  end
+end
+
+describe "GetLastError" do
+  it "get last erro " do
+    Win32ft.GetFileType -100
+    Win32ft.GetLastError.should == 6
+  end
+end
+
+describe "FileTime LocalFileTime" do
+  it "test swap filetime localfiletime" do
+    ft1 = FileTime.new
+    ft1[:dwHighDateTime], ft1[:dwLowDateTime] = 0x1CCF838, 0x1F2B7320
+    ft2 = Win32ft.ft2lft(ft1)
+    ft3 = Win32ft.lft2ft(ft2)
+    
+    ft1[:dwHighDateTime].should == ft3[:dwHighDateTime]
+    ft1[:dwLowDateTime].should == ft3[:dwLowDateTime]
+  end
+end
+
+describe "FileTime SystemTime" do
+  it "test swap filetime systemtime" do
+    ft1 = FileTime.new
+    ft1[:dwHighDateTime], ft1[:dwLowDateTime] = 0x01CCF838, 0x1F2B7320
+    st1 = Win32ft.ft2st(ft1)
+    ft3 = Win32ft.st2ft(st1)
+    ft1[:dwHighDateTime].should == ft3[:dwHighDateTime]
+    ft1[:dwLowDateTime].should == ft3[:dwLowDateTime]
+  end
+end
+
+describe "GetSystemTime" do
+  it "getsystemtime is_a? SystemTime" do
+    t1 = Win32ft.getsystemtime
+    t1.should satisfy { |st| st.is_a? SystemTime}
+  end
+  it "getsystemtime [:wYear] == current year" do
+    t1 = Win32ft.getsystemtime
+    t1[:wYear].should == Time.now.year
+  end
+end
+
+describe "GetLocalTime" do
+  it "getlocaltime is_a? SystemTime" do
+    t1 = Win32ft.getlocaltime
+    t1.should satisfy { |st| st.is_a? SystemTime}
+  end
+  it "getlocaltime [:wYear] == current year" do
+    t1 = Win32ft.getlocaltime
+    t1[:wYear].should == Time.now.year
+  end
+end
+
+describe "GetLocalTime GetSystemTime" do
+  it "diff localtime systemtime" do
+    st1 = Win32ft.getsystemtime
+    lt1 = Win32ft.getlocaltime
+    t1 = Time.utc st1[:wYear], st1[:wMonth], st1[:wDay], st1[:wHour], 
+                  st1[:wMinute], st1[:wSecond]
+    t2 = Time.local lt1[:wYear], lt1[:wMonth], lt1[:wDay], lt1[:wHour], 
+                  lt1[:wMinute], lt1[:wSecond]
+    t1.should == t2
+  end
+end
+
+
+describe "File Create Read Write Flush Close GetFileSizeEx" do
+  before(:each) do
+    @fn1 = "c:\\f1.txt"
+    @fn2 = "c:\\f2.txt"
+    @msg = Time.now.to_s + " asfas f;asjf;lasdfj;s af;alsj f"
+  end
+    
+  it "CreateFileA WriteFile CloseHandle GetFileSizeEx" do
+    hf = Win32ft.CreateFileA(@fn1, CFflag::GENERIC_WRITE,
+       CFflag::FILE_SHARE_READ | CFflag::FILE_SHARE_WRITE,
+       nil, CFflag::OPEN_ALWAYS, 0, 0)
+    hf.should satisfy { |obj| obj.is_a? Fixnum }
+       
+    wded = FFI::MemoryPointer.new(:uint32, 1)
+    buffer = FFI::MemoryPointer.new(:char, @msg.bytesize)
+    buffer.write_string @msg
+    wfres = Win32ft.WriteFile(hf, buffer, @msg.bytesize, wded, nil)
+    wfres.should be(true)
+    wded.read_uint32.should == @msg.bytesize
+    
+    chres = Win32ft.CloseHandle(hf)
+    chres.should be(true)
+  end
+  
+  it "ReadFile" do
+    buffer = FFI::MemoryPointer.new :char, @msg.bytesize*2
+    rded = FFI::MemoryPointer.new :uint32, 1
+    hf = Win32ft.CreateFileA(@fn1, CFflag::GENERIC_READ,
+       CFflag::FILE_SHARE_READ | CFflag::FILE_SHARE_WRITE,
+       nil, CFflag::OPEN_EXISTING, 0, 0)
+    hf.should satisfy { |obj| obj.is_a? Fixnum }
+    rfres = Win32ft.ReadFile(hf, buffer, @msg.bytesize*2, rded, nil)
+    rfres.should be(true)
+    rded.read_uint32.should == @msg.bytesize
+    buffer.read_string.should == @msg
+    Win32ft.CloseHandle(hf)
+  end
+  
+  it "ReadFile no exist file" do
+    fn = "c:\\noexist.txt"
+    hf = Win32ft.CreateFileA(fn, CFflag::GENERIC_READ,
+       CFflag::FILE_SHARE_READ | CFflag::FILE_SHARE_WRITE,
+       nil, CFflag::OPEN_EXISTING, 0, 0)
+    hf.should == -1
+  end
+  
+  it "getfilesize" do
+    size = Win32ft.getfilesize(@fn1)
+    size.should == @msg.bytesize
+  end
+end
+
+describe "GetFileTime SetFileTime" do
+  before(:each) do
+    @fn1 = "c:\\f1.txt"
+  end
+  
+  it "getfiletime" do
+    tc1, ta1, tm1 = Win32ft.getfiletime(@fn1)
+    tc1.should_not == FileTime.new
+    ta1.should_not == FileTime.new
+    tm1.should_not == FileTime.new
+  end
+  
+  it "setfiletime" do
+    require 'tempfile'
+    tc1, ta1, tm1 = Win32ft.getfiletime(@fn1)
+    fnt = Tempfile.new 'test'
+    fnt.puts Time.now.to_s
+    fnt.close
+
+    tc2, ta2, tm2 = Win32ft.getfiletime(fnt.path)
+    tc2.should_not == tc1
+    ta2.should_not == ta1
+    tm2.should_not == tm1
+    
+    res = Win32ft.setfiletime(fnt.path, tc1, ta1, tm1)
+    res.should be(true)
+    tc3, ta3, tm3 = Win32ft.getfiletime(fnt.path)
+    tc3.should == tc1
+    ta3.should == tc1
+    tm3.should == tm1
+  end
+  
+  it "ft2double double2ft" do
+    tc1, ta1, tm1 = Win32ft.getfiletime(@fn1)
+    ftc1 = Win32ft.ft2double(tc1)
+    fta1 = Win32ft.ft2double(ta1)
+    ftm1 = Win32ft.ft2double(tm1)
+    tc2 = Win32ft.double2ft(ftc1)
+    ta2 = Win32ft.double2ft(fta1)
+    tm2 = Win32ft.double2ft(ftm1)
+    (tc2[:dwLowDateTime] - tc1[:dwLowDateTime]).should <= 10
+    (ta2[:dwLowDateTime] - ta1[:dwLowDateTime]).should <= 10
+    (tm2[:dwLowDateTime] - tm1[:dwLowDateTime]).should <= 10
+  end
+end
---
+++ win32ft.rb
@@ -0,0 +1,283 @@
+#!/usr/bin/env ruby -wKU
+# encoding: GBK
+
+require "ffi"
+
+class FileTime < FFI::Struct
+  layout :dwLowDateTime, FFI::Type::UINT32,
+          :dwHighDateTime, FFI::Type::UINT32
+  
+  def ==(other)
+    if other.is_a? self.class
+      self[:dwLowDateTime] == other[:dwLowDateTime] && self[:dwHighDateTime] == other[:dwHighDateTime]
+    elsif other.is_a? Numeric
+      self[:dwHighDateTime] << 32 | self[:dwLowDateTime] == other
+    else
+      false
+    end
+  end
+  def equal?(other)
+    self == other
+  end
+  def eql?(other)
+    self == other
+  end
+  def to_s
+    "0x%08X%08X" % [self[:dwHighDateTime], self[:dwLowDateTime]]
+  end
+  def to_ii
+    ((self[:dwHighDateTime] << 32 | self[:dwLowDateTime]) - 116444736000000000) / 10**7.0
+  end
+end
+
+class SystemTime < FFI::Struct
+  layout :wYear, FFI::Type::USHORT,
+          :wMonth, FFI::Type::USHORT,
+          :wDayOfWeek, FFI::Type::USHORT,
+          :wDay, FFI::Type::USHORT,
+          :wHour, FFI::Type::USHORT,
+          :wMinute, FFI::Type::USHORT,
+          :wSecond, FFI::Type::USHORT,
+          :wMilliseconds, FFI::Type::USHORT
+
+  def ==(other)
+    if other.is_a? self.class
+      self[:wYear] == other[:wYear] &&
+      self[:wMonth] == other[:wMonth] &&
+      self[:wDay] == other[:wDay] &&
+      self[:wHour] == other[:wHour] &&
+      self[:wMinute] == other[:wMinute] &&
+      self[:wSecond] == other[:wSecond] &&
+      self[:wMilliseconds] == other[:wMilliseconds]
+    elsif other.is_a? String
+      self.to_s == other
+    else
+      false
+    end
+  end
+  def equal?(other)
+    self == other
+  end
+  def eql?(other)
+    self == other
+  end
+  def to_s
+    "%04d-%02d-%02d %02d:%02d:%02d.%d" % [
+      self[:wYear],self[:wMonth],self[:wDay],self[:wHour],self[:wMinute],self[:wSecond],self[:wMilliseconds]
+    ]
+  end
+end
+
+class Large_Integer < FFI::Struct
+  layout :LowPart, FFI::Type::UINT32,
+          :HighPart, FFI::Type::UINT32
+
+  def ==(other)
+    if other.is_a? self.class
+      self[:HighPart] == other[:HighPart] && self[:LowPart] == other[:LowPart]
+    elsif other.is_a? Numeric
+      self[:HighPart] << 32 | self[:LowPart] == other
+    else
+      false
+    end
+  end
+  def equal?(other)
+    self == other
+  end
+  def eql?(other)
+    self == other
+  end
+  def to_i
+    self[:HighPart] << 32 | self[:LowPart]
+  end
+  def to_s
+    to_i.to_s
+  end
+end
+
+class HANDLE < FFI::Struct
+  layout :handle, FFI::Type::UINT32
+end
+
+class CFflag
+  GENERIC_READ      = 0x80000000
+  GENERIC_WRITE     = 0x40000000
+  GENERIC_EXECUTE   = 0x20000000
+  GENERIC_ALL       = 0x10000000
+
+  FILE_SHARE_READ   = 0x00000001
+  FILE_SHARE_WRITE  = 0x00000002
+  FILE_SHARE_DELETE = 0x00000004
+
+  CREATE_NEW        = 1
+  CREATE_ALWAYS     = 2
+  OPEN_EXISTING     = 3
+  OPEN_ALWAYS       = 4
+  TRUNCATE_EXISTING = 5
+
+  FILE_FLAG_WRITE_THROUGH         =0x80000000
+  FILE_FLAG_OVERLAPPED            =0x40000000
+  FILE_FLAG_NO_BUFFERING          =0x20000000
+  FILE_FLAG_RANDOM_ACCESS         =0x10000000
+  FILE_FLAG_SEQUENTIAL_SCAN       =0x08000000
+  FILE_FLAG_DELETE_ON_CLOSE       =0x04000000
+  FILE_FLAG_BACKUP_SEMANTICS      =0x02000000
+  FILE_FLAG_POSIX_SEMANTICS       =0x01000000
+  FILE_FLAG_OPEN_REPARSE_POINT    =0x00200000
+  FILE_FLAG_OPEN_NO_RECALL        =0x00100000
+  FILE_FLAG_FIRST_PIPE_INSTANCE   =0x00080000
+end
+
+module Win32ft
+  extend FFI::Library
+  ffi_lib 'msvcrt', 'kernel32'
+  ffi_convention :stdcall
+  
+  attach_function :GetFileType, [:int], :int
+  attach_function :GetLastError, [], :int
+  attach_function :FormatMessageA, [:int, :pointer, :int, :int, :string, :int, :pointer], :int
+  
+  attach_function :FileTimeToLocalFileTime, [FileTime.by_ref, FileTime.by_ref], :bool
+  attach_function :LocalFileTimeToFileTime, [FileTime.by_ref, FileTime.by_ref], :bool
+  def self.ft2lft(ft)
+    lft = FileTime.new
+    FileTimeToLocalFileTime(ft, lft)
+    lft
+  end
+  def self.lft2ft(lft)
+    ft = FileTime.new
+    LocalFileTimeToFileTime(lft, ft)
+    ft
+  end
+  
+  attach_function :FileTimeToSystemTime, [FileTime.by_ref, SystemTime.by_ref], :bool
+  attach_function :SystemTimeToFileTime, [SystemTime.by_ref, FileTime.by_ref], :bool
+  def self.ft2st(ft, convft2lft: false)
+    ft = ft2lft(ft) if convft2lft
+    st = SystemTime.new
+    FileTimeToSystemTime(ft, st)
+    st
+  end
+  def self.st2ft(st, convlft2ft: false)
+    ft = FileTime.new
+    SystemTimeToFileTime(st, ft)
+    ft = lft2ft(ft) if convlft2ft
+    ft
+  end
+  
+  attach_function :GetSystemTime, [SystemTime.by_ref], :void
+  def self.getsystemtime
+    st = SystemTime.new
+    GetSystemTime(st)
+    st
+  end
+  attach_function :GetLocalTime, [SystemTime.by_ref], :void
+  def self.getlocaltime
+    lt = SystemTime.new
+    GetLocalTime(lt)
+    lt
+  end
+  
+=begin
+CreateFileA("", CF.GENERIC_READ,  CF.FILE_SHARE_READ | CF.FILE_SHARE_WRITE, 0, CF.OPEN_EXISTING, CF.FILE_FLAG_BACKUP_SEMANTICS, 0)
+CreateFileA("", CF.GENERIC_WRITE, CF.FILE_SHARE_READ | CF.FILE_SHARE_WRITE, 0, CF.OPEN_EXISTING, CF.FILE_FLAG_BACKUP_SEMANTICS, 0)
+
+   LPCSTR  lpFileName,             # "filename"
+   DWORD   dwDesiredAccess,        # GENERIC_READ / GENERIC_WRITE / GENERIC_EXECUTE / GENERIC_ALL
+   DWORD   dwShareMode,            # FILE_SHARE_READ /| FILE_SHARE_WRITE / FILE_SHARE_DELETE
+   LPSECURITY_ATTRIBUTES   lp,     # 0
+   DWORD   dwCreationDisposition,  # CREATE_NEW / CREATE_ALWAYS / OPEN_EXISTING / OPEN_ALWAYS / TRUNCATE_EXISTING
+   DWORD   dwFlagsAndAttributes,   # 0 | CF.FILE_FLAG_BACKUP_SEMANTICS if dir
+   HANDLE  hTemplateFile           # 0
+       
+ReadFile(
+    HANDLE,
+    ctypes.byref(sts),  # ctypes.create_string_buffer('',size=100).  chars
+    100,    # chars len
+    ctypes.byref(rd),   # ctypes.wintypes.DWORD(). # [output] read chars len
+    0)
+
+WriteFile(
+    HANDLE,
+    ctypes.byref(sts),  # ctypes.create_string_buffer('abcde',size=100).  chars
+    5,    # chars len
+    ctypes.byref(wd),   # ctypes.wintypes.DWORD(). # [output] write chars len
+    0)
+
+FlushFileBuffers(HANDLE)
+
+CloseHandle(HANDLE)
+=end
+  attach_function  :CreateFileA,  [FFI::Type::STRING, FFI::Type::UINT32,
+       FFI::Type::UINT32, :pointer, FFI::Type::UINT32,
+       FFI::Type::UINT32, FFI::Type::UINT32], FFI::Type::INT32
+  attach_function :ReadFile, [FFI::Type::INT32, :pointer, :uint32, :pointer, :pointer], :bool
+  attach_function :WriteFile, [FFI::Type::INT32, :pointer, :uint32, :pointer, :pointer], :bool
+  attach_function :FlushFileBuffers, [FFI::Type::INT32], :bool
+  attach_function :CloseHandle, [FFI::Type::INT32], :bool
+  
+  attach_function :GetFileTime, [FFI::Type::INT32, FileTime.by_ref, FileTime.by_ref, FileTime.by_ref], :bool
+  attach_function :SetFileTime, [FFI::Type::INT32, FileTime.by_ref, FileTime.by_ref, FileTime.by_ref], :bool
+  def self.getfiletime(fn)
+    tc, ta, tm = FileTime.new, FileTime.new, FileTime.new
+    ttts = [tc, ta, tm]
+    hf = Win32ft.CreateFileA(fn, CFflag::GENERIC_READ, CFflag::FILE_SHARE_READ | CFflag::FILE_SHARE_WRITE,
+        nil, CFflag::OPEN_EXISTING, CFflag::FILE_FLAG_BACKUP_SEMANTICS, 0)
+    if hf == -1
+      STDERR.puts "Can not open file \"#{fn}\""
+      return ttts
+    end
+    res = Win32ft.GetFileTime(hf, tc, ta, tm)
+    Win32ft.CloseHandle(hf)
+    if !res
+      STDERR.puts "GetFileTime error."
+      return ttts
+    end
+    ttts
+  end
+  def self.setfiletime(fn, tc, ta, tm)
+    hf = CreateFileA(fn, CFflag::GENERIC_WRITE, CFflag::FILE_SHARE_READ | CFflag::FILE_SHARE_WRITE,
+        nil, CFflag::OPEN_EXISTING, CFflag::FILE_FLAG_BACKUP_SEMANTICS, 0)
+    if hf == -1
+      STDERR.puts "Can not open file \"#{fn}\""
+      return false
+    end
+    res = Win32ft.SetFileTime(hf, tc, ta, tm)
+    if !res
+      STDERR.puts "SetFileTime error."
+      return false
+    end
+    CloseHandle(hf)
+    true
+  end
+  def self.double2ft(tt)
+    wintt = (tt * 10**7 + 116444736000000000).to_i
+    ft = FileTime.new
+    ft[:dwHighDateTime] = wintt >> 32 & 0xFFFFFFFF
+    ft[:dwLowDateTime] = wintt & 0xFFFFFFFF
+    ft
+  end
+  def self.ft2double(ft)
+    wintt = ft[:dwHighDateTime] << 32 | ft[:dwLowDateTime]
+    tt = (wintt - 116444736000000000) / 10**7.0
+    tt
+  end
+  
+  attach_function :GetFileSizeEx, [FFI::Type::INT32, Large_Integer.by_ref], :bool
+  def self.getfilesize(fn)
+    hf = CreateFileA(fn, CFflag::GENERIC_READ, CFflag::FILE_SHARE_READ | CFflag::FILE_SHARE_WRITE,
+        nil, CFflag::OPEN_EXISTING, CFflag::FILE_FLAG_BACKUP_SEMANTICS, 0)
+    if hf == -1
+      STDERR.puts "Can not open file \"#{fn}\""
+      return false
+    end
+    size = Large_Integer.new
+    res = Win32ft.GetFileSizeEx(hf, size)
+    if !res
+      STDERR.puts "GetFileSizeEx error."
+      return false
+    end
+    CloseHandle(hf)
+    size.to_i
+  end
+end
