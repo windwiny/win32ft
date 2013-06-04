45264a97f76a7743b3eef9452f078b80c511fa38
windwiny <windwiny.ubt@gmail.com>
2013-06-04 08:50:36 +0800

    get dir times

---
+++ getdirtimes.rb
@@ -0,0 +1,66 @@
+#!/usr/bin/env ruby -wKU
+# encoding: GBK
+
+require_relative 'win32ft'
+
+G = {:autorun => false, :stdout => STDOUT}
+
+trap "SIGINT" do
+  STDERR.puts "exit on Ctrl-C.  ids: #{$ids}  ifs: #{$ifs}"
+  exit 1
+end
+
+if !STDOUT.tty? && STDERR.tty?
+  G[:prprog] = true
+  G[:BS] = "\b" * 60
+  G[:WS] = " " * 60
+  def tee(txt, both: true)
+    puts txt
+    STDERR.print txt if both
+  end
+else
+  G[:prprog] = false
+  def tee(txt)
+    puts txt
+  end
+end
+
+def prdi(di)
+  Dir.chdir(di)
+  $ids, $ifs = 0, 0
+  t00 = t0 = Time.now
+  Dir.glob('**/*').each do |fn|
+    if File.directory? fn
+      $ids += 1
+      tc, ta, tm = Win32ft.getfiletime(fn)
+      puts "#{fn}/ #{tc} #{ta} #{tm} 0"
+    elsif File.file? fn
+      $ifs += 1
+      tc, ta, tm, sz= Win32ft.getfiletime(fn, getsize: true)
+      puts "#{fn} #{tc} #{ta} #{tm} #{sz}"
+    end
+    if G[:prprog]
+      ts = Time.now
+      if ts - t0 > 0.3
+        STDERR.print "#{G[:BS]} Dir: #{di}  ds: #{$ids}  fs: #{$ifs}  time: #{ts - t00}"
+        t0 = ts
+      end
+    end
+  end
+  if G[:prprog]
+    ts = Time.now
+    STDERR.puts "#{G[:BS]} Dir: #{di}  ds: #{$ids}  fs: #{$ifs}  time: #{ts - t00}"
+  end
+end
+
+def main
+  ARGV.each do |fn|
+    if File.directory? fn
+      prdi(fn)
+    end
+  end
+end
+
+if __FILE__ == $0
+  main
+end
